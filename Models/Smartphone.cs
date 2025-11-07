c66028177b89d3532cfdc9290b1c61e9318ce44c
boscojoao <59241573+bosco270@users.noreply.github.com>
2025-11-06 23:13:58 -0300

    Enhance Smartphone class with new properties and methods
    Updated the Smartphone class constructor to include additional parameters for model, IMEI, and memory. Added a method to display smartphone details.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,12 +2,21 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        // ----- atributos
         public string Numero { get; set; }
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
+
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -19,6 +28,13 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        public void Dados()
+        {
+            Console.WriteLine($" Numero : {Numero} \n"
+                            + $" Modelo : {Modelo} \n"
+                            + $" IMEI   : {IMEI} \n"
+                            + $" Memoria: {Memoria} \n");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
